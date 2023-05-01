bf58d4c6336470adfbd3b37277bac554bc506284
Ahmad Jundi Rabbani <101819376+ahmadjundirabbani@users.noreply.github.com>
2023-05-01 17:47:05 +0700

    Create Episode 02 - Tipe Data .py

---
+++ Episode 02 - Tipe Data .py
@@ -0,0 +1,30 @@
+# a = 10, a adalah variabelnya, 10 adalah nilai dari variabel a
+
+## Tipe Data
+
+# Satuan angka yang tidak ada komanya (integer)
+data_integer = 11
+print("Data : ", data_integer) # data_integer di baris ini untuk memanggil variabel tersebut dan diluar dari tanda kutip itu supaya tidak jadi string
+print("Bertipe : ", type(data_integer) # type ini untuk mengecek type data yang ada di variabel tersebut
+      
+# Satuan angka mempunyai koma (float)
+data_float = 1.5 # koma dikoding menggunakan titik (.) bukan (,)
+print("Data : ", data_float)
+print("Bertipe : ", type(data_float)
+      
+# Kumpulan karakter (string)
+data_string = "jundi" # menggunakan tanda kutip diawal dan diakhir nya
+print("Data : ", data_string)
+print("Bertipe : ", type(data_string)
+   
+# Biner antara True/False (boolean)
+data_bool = false # digunakan jika ada 2 kondisi
+print("Data : ", data_bool)
+print("Bertipe : ", type(data_bool)
+      
+## Tipe Data Khusus
+      
+# bilangan kompleks
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe : ", type(data_complex))

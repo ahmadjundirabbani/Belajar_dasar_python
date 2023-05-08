5152ea9d6e455b7997528408628c29350eed633d
Ahmad Jundi Rabbani <101819376+ahmadjundirabbani@users.noreply.github.com>
2023-05-08 14:10:11 +0700

    Create Episode 19 - Elif Statement .py

---
+++ Episode 19 - Elif Statement .py
@@ -0,0 +1,14 @@
+# ELIF = else if statement 
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "ucup": # kondisi 1
+  print("Hai ucup!") # aksi true 1
+elif nama == "otong": # kondisi 2
+  print("eh si otong") # aksi true 2
+elif nama == "budi": # kondisi 3
+  print("mana bapak budi") # aksi true 3
+else:
+  print("au ah ga kenall sama lu") # aksi false semua
+  
+print("ini adalah akhir dari program")
